f5362f8eddfe9e3447d3bcbadce5955e06060fb4
Kbenjak <kbenjak@outlook.com>
2024-05-27 09:21:48 +0200

    Add files via upload

---
+++ Webappcreation.ps1
@@ -0,0 +1,11 @@
+﻿# Name of the resource group you have created in Azure tenant
+$resourceGroupName = "MyResourceGroup"
+# Name of the Web App you want to create
+$WebAppName = "MyWebApp2024994"
+# Region where all other resources are created
+$Region = "Italy North"
+# Service Plan in which all other applications were created
+$AppServicePlan = "ASP-MyResourceGroup-ae1f"
+
+# Command for new app creation
+New-AzWebApp -ResourceGroupName $resourceGroupName -Name $WebAppName -Location $Region -AppServicePlan $AppServicePlan
